c5c92f2f4382b401307f28d6eed83196d3d6b96f
slavahawk <krylov_s10@mail.ru>
2020-07-06 14:47:27 +0700

    123

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,9 +1,8 @@
-@@include('jquery.js');
 @@include('slick.min.js');
+@@include('popup.js');
 @@include('webp.js');
 @@include('burger.js');
 @@include('mainSlider.js');
+@@include('map.js');
 @@include('productSlider.js');
 @@include('stockstSlider.js');
-@@include('popup.js');
-@@include('map.js');
