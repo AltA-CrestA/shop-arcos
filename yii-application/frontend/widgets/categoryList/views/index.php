8404aed1df8a89a34a62a71006db804929013c38
Alexey <info@webskill24.ru>
2020-07-07 17:35:24 +0300

    actionCategory Link made!

--- yii-application/frontend/widgets/categoryList/views/index.php
+++ yii-application/frontend/widgets/categoryList/views/index.php
@@ -4,5 +4,5 @@ use yii\helpers\Url;
 
 <h6>Категории</h6>
 <?php foreach ($categoryList as $category): ?>
-    <a href="<?php echo Url::to(['catalog/category', 'categoryId' => $category->id]); ?>"><?php echo $category->name; ?></a>
+    <a href="<?php echo Url::to(['catalog/category', 'categoryId' => $category->id]); ?>" class="<?php echo (Url::current() == Url::to(['catalog/category', 'categoryId' => $category->id])) ? 'active' : '' ;?>"><?php echo $category->name; ?></a>
 <?php endforeach; ?>
